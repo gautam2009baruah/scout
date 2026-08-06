0527bc46a8630df46bb41ca7fcd393f7a7c2f356
Baruah, Gautam <Gautam.Baruah@sabre.com>
2026-08-06 10:07:33 +0000

    Remove leftover commented BullEngine references in scheduler factory

--- lib/orchestrations/scheduler/factory.ts
+++ lib/orchestrations/scheduler/factory.ts
@@ -5,9 +5,6 @@
 import type { ISchedulerEngine, ScheduleCallback } from "./types";
 import { NodeCronEngine } from "./node-cron-engine";
 
-// Future: Import BullEngine when implemented
-// import { BullEngine } from "./bull-engine";
-
 export type SchedulerEngineType = "node-cron" | "bull";
 
 /**
@@ -26,11 +23,6 @@ export function createSchedulerEngine(callback: ScheduleCallback): ISchedulerEng
       return new NodeCronEngine(callback);
 
     case "bull":
-      // Future implementation
-      // if (!process.env.REDIS_URL) {
-      //   throw new Error("REDIS_URL is required for Bull Queue engine");
-      // }
-      // return new BullEngine(callback, process.env.REDIS_URL);
       throw new Error("Bull Queue engine not yet implemented. Use SCHEDULER_ENGINE=node-cron");
 
     default:
